25c4eda514f65d314eb60e2ada2691f40dc980db
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-04-26 21:30:50 +0700

    add 'create token api'

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,18 @@
 from rest_framework import generics
-from .serializers import UserSeializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from .serializers import UserSeializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSeializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+
+    # biar bisa diakses dari browser, kalau endak harus pake postman
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
