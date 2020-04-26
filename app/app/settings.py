8a680a511494411cfe8f50d6608a799b8fc7f076
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-04-26 15:47:57 +0700

    Change secret,debug,and allow host config to use env

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,13 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q4wd=807qbnqq_ez=o8elhihn1fs+umx#puz=5(iqck)l!!0b5'
+# SECRET_KEY = 'q4wd=807qbnqq_ez=o8elhihn1fs+umx#puz=5(iqck)l!!0b5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
 
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
